e503952cbeb1d90d2d9d4484ff50add1ed7892bd
Luigi Bellomarini <bellomarini@yahoo.it>
2016-05-12 06:22:02 -0700

    New examples.

--- EXAMPLES/example_9.sql
+++ EXAMPLES/example_9.sql
@@ -7,7 +7,7 @@ DECLARE
     -- Oracle target schema
     V_TARGET_SCHEMA varchar2(20) := 'GAIA_INPUT';
     -- Semicolon-separated schema mappings of the transformation scenario
-    V_MAPPING_LIST varchar2(200) := 'BANKS(i,x,y,z,k)->G1(i,x),G2(i,y),G3(i,z),G4(i,k)';
+    V_MAPPING_LIST varchar2(200) := 'BANKS(i,x,y,z,k)->G1(i,x),G2(i,y),G3(i,z)';
 BEGIN
     GAIA.encode(V_MAPPING_LIST,V_SOURCE_SCHEMA,V_TARGET_SCHEMA,v_mapping_set_out, false);
 END;
